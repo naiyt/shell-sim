f6bdfdd337ea39bc4a5188c9576aa64d1fc33b7d
Nate Collings <nate@natecollings.com>
2015-09-30 22:25:26 -0600

    explicitly load all commands

--- lib/shell_sim.rb
+++ lib/shell_sim.rb
@@ -7,6 +7,24 @@ require 'shell_sim/shell'
 require 'shell_sim/user'
 require 'shell_sim/scripting'
 
+# TODO: figure out why I can't glob load these
+require 'shell_sim/commands/cat'
+require 'shell_sim/commands/cd'
+require 'shell_sim/commands/echo'
+require 'shell_sim/commands/exit'
+require 'shell_sim/commands/filetype'
+require 'shell_sim/commands/grep'
+require 'shell_sim/commands/help'
+require 'shell_sim/commands/history'
+require 'shell_sim/commands/login'
+require 'shell_sim/commands/ls'
+require 'shell_sim/commands/man'
+require 'shell_sim/commands/mkdir'
+require 'shell_sim/commands/pwd'
+require 'shell_sim/commands/rmdir'
+require 'shell_sim/commands/task'
+require 'shell_sim/commands/touch'
+
 module ShellSim
   def self.configure
     yield config
